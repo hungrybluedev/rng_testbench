6ff13154ffb48ede52ada5cccfe88b7d433f18ab
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-04-14 10:26:04 +0530

    add external RNGs

---
+++ myrngs/minstd/minstd.v
@@ -0,0 +1,98 @@
+module minstd
+
+import rand.seed
+
+pub const seed_len = 1
+
+pub struct MinStdRNG {
+mut:
+	seed       int = int(seed.time_seed_32() & 0x7fffffff)
+	buffer     int
+	bytes_left int
+}
+
+const (
+	max = 2147483647
+	a   = 48271
+	q   = 44488
+	r   = 3399
+)
+
+// seed is used to seed the RNG.
+pub fn (mut rng MinStdRNG) seed(seed_data []u32) {
+	if seed_data.len != minstd.seed_len {
+		panic('seed data must have 1 element')
+	}
+	rng.seed = int(seed_data[0] & 0x7fffffff)
+	rng.bytes_left = 0
+	rng.buffer = 0
+}
+
+fn (mut rng MinStdRNG) internal_int() int {
+	rng.seed = minstd.a * (rng.seed % minstd.q) - minstd.r * (rng.seed / minstd.q)
+	rng.seed += if rng.seed < 0 { minstd.max } else { 0 }
+	return rng.seed
+}
+
+// byte returns a uniformly distributed pseudorandom 8-bit unsigned positive `byte`.
+[inline]
+pub fn (mut rng MinStdRNG) byte() byte {
+	if rng.bytes_left >= 1 {
+		rng.bytes_left -= 1
+		value := byte(rng.buffer)
+		rng.buffer >>= 8
+		return value
+	}
+	rng.buffer = rng.internal_int()
+	rng.bytes_left = 2
+	value := byte(rng.buffer)
+	rng.buffer >>= 8
+	return value
+}
+
+// u16 returns a pseudorandom 16-bit unsigned integer (`u16`).
+[inline]
+pub fn (mut rng MinStdRNG) u16() u16 {
+	if rng.bytes_left >= 2 {
+		rng.bytes_left -= 2
+		value := u16(rng.buffer)
+		rng.buffer >>= 16
+		return value
+	}
+	ans := rng.internal_int()
+	rng.buffer = ans >> 16
+	rng.bytes_left = 1
+	return u16(ans)
+}
+
+// u32 returns a pseudorandom 32-bit unsigned integer (`u32`).
+[inline]
+pub fn (mut rng MinStdRNG) u32() u32 {
+	return (u32(rng.internal_int()) << 1) | u32(rng.internal_int() & 1)
+}
+
+// u64 returns a pseudorandom 64-bit unsigned integer (`u64`).
+[inline]
+pub fn (mut rng MinStdRNG) u64() u64 {
+	return (u64(rng.u32()) << 32) | rng.u32()
+}
+
+// block_size returns the number of bits that the RNG can produce in a single iteration.
+pub fn (mut rng MinStdRNG) block_size() int {
+	return 32
+}
+
+// free should be called when the generator is no longer needed
+[unsafe]
+pub fn (mut rng MinStdRNG) free() {
+	unsafe { free(rng) }
+}
+
+// int minstd(void) {
+// seed = A * (seed % Q) - R * (seed / Q);
+// seed += (seed < 0) ? M : 0;
+// return seed;
+// }
+// double dminstd(void) {
+// return minstd() / (double)M;
+// }
---
+++ myrngs/xoshiro/xoshiro256plusplus.v
@@ -0,0 +1,106 @@
+module xoshiro
+
+import rand.seed
+
+pub const seed_len = 8
+
+pub struct X256PlusPlusRNG {
+mut:
+	state []u64 = [seed.time_seed_64(), seed.time_seed_64(), seed.time_seed_64(),
+	seed.time_seed_64()]
+	buffer     u64
+	bytes_left int
+}
+
+// seed is used to seed the RNG.
+pub fn (mut rng X256PlusPlusRNG) seed(seed_data []u32) {
+	if seed_data.len != xoshiro.seed_len {
+		panic('seed data must have 8 elements')
+	}
+	rng.state[0] = u64(seed_data[0]) | (u64(seed_data[1]) << 32)
+	rng.state[1] = u64(seed_data[2]) | (u64(seed_data[3]) << 32)
+	rng.state[2] = u64(seed_data[4]) | (u64(seed_data[5]) << 32)
+	rng.state[3] = u64(seed_data[6]) | (u64(seed_data[7]) << 32)
+	rng.bytes_left = 0
+	rng.buffer = 0
+}
+
+// byte returns a uniformly distributed pseudorandom 8-bit unsigned positive `byte`.
+[inline]
+pub fn (mut rng X256PlusPlusRNG) byte() byte {
+	if rng.bytes_left >= 1 {
+		rng.bytes_left -= 1
+		value := byte(rng.buffer)
+		rng.buffer >>= 8
+		return value
+	}
+	rng.buffer = rng.u64()
+	rng.bytes_left = 7
+	value := byte(rng.buffer)
+	rng.buffer >>= 8
+	return value
+}
+
+// u16 returns a pseudorandom 16-bit unsigned integer (`u16`).
+[inline]
+pub fn (mut rng X256PlusPlusRNG) u16() u16 {
+	if rng.bytes_left >= 2 {
+		rng.bytes_left -= 2
+		value := u16(rng.buffer)
+		rng.buffer >>= 16
+		return value
+	}
+	ans := rng.u64()
+	rng.buffer = ans >> 16
+	rng.bytes_left = 6
+	return u16(ans)
+}
+
+// u32 returns a pseudorandom 32-bit unsigned integer (`u32`).
+[inline]
+pub fn (mut rng X256PlusPlusRNG) u32() u32 {
+	if rng.bytes_left >= 2 {
+		rng.bytes_left -= 2
+		value := u32(rng.buffer)
+		rng.buffer >>= 32
+		return value
+	}
+	ans := rng.u64()
+	rng.buffer = ans >> 32
+	rng.bytes_left = 4
+	return u32(ans)
+}
+
+// u64 returns a pseudorandom 64-bit unsigned integer (`u64`).
+[direct_array_access; inline]
+pub fn (mut rng X256PlusPlusRNG) u64() u64 {
+	result := rotl(rng.state[0] + rng.state[3], 23) + rng.state[0]
+
+	t := rng.state[1] << 17
+
+	rng.state[2] ^= rng.state[0]
+	rng.state[3] ^= rng.state[1]
+	rng.state[1] ^= rng.state[2]
+	rng.state[0] ^= rng.state[3]
+
+	rng.state[2] ^= t
+
+	rng.state[3] = rotl(rng.state[3], 45)
+
+	return result
+}
+
+fn rotl(x u64, k int) u64 {
+	return (x << k) | (x >> (64 - k))
+}
+
+// block_size returns the number of bits that the RNG can produce in a single iteration.
+pub fn (mut rng X256PlusPlusRNG) block_size() int {
+	return 64
+}
+
+// free should be called when the generator is no longer needed
+[unsafe]
+pub fn (mut rng X256PlusPlusRNG) free() {
+	unsafe { free(rng) }
+}
